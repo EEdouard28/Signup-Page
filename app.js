e47be562222ff23413b31efcfce2e9b76ee19c3c
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-10-27 17:20:30 -0400

    html and server updates

--- app.js
+++ app.js
@@ -6,6 +6,28 @@ const request = require("request");
 
 //Server setup
 const app = express();
+
+//Path of local static files
+app.use(express.static("public"));
+
+//Use of bodyparser or express
+// app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+//send the file signup html
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html")
+})
+
+//Post route created... path home and callback function
+app.post("/", function(req, res) {
+    let firstName = req.body.fName;
+    let lastName = req.body.lName;
+    let email= req.body.email;
+
+    console.log(firstName, lastName, email);
+})
+
 app.listen(3000, function() {
     console.log("Server is running on port 3000");
 });
